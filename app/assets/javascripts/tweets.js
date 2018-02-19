40f6121a9d7b146fe49ee259191f440077db2d99
jeffrothwell <rothwelljeff@gmail.com>
2018-02-19 18:09:47 -0500

    attempt to re-enable tweet button after each submission - NOT WORKING YET

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -2,8 +2,13 @@
 // All this logic will automatically be available in application.js.
 document.addEventListener('DOMContentLoaded', function(){
 
+  console.log("I'm loaded");
+
   var tweetUlEl = document.querySelector('.tweets');
   var tweetFormEl = document.querySelector('#new_tweet');
+  var tweetSubmitEl = document.querySelector('#create-tweet');
+
+  console.log(tweetSubmitEl);
 
   tweetFormEl.addEventListener('submit', function(e){
     e.preventDefault();
@@ -23,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function(){
       newLi.append(timeEl);
       newLi.classList.add('tweet');
       tweetUlEl.prepend(newLi);
+      tweetSubmitEl.disabled = false;
     }).fail(function(_,b,c){
       console.log('failed to tweet');
     })
